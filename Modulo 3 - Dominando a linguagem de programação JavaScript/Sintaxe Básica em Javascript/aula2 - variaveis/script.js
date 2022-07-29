31c68aa8679111982b306a418211334fcd297c66
Pedro <pedro-fernandes25@hotmail.com>
2022-07-29 15:29:25 -0300

    Sintaxe Básica em Javascript

---
+++ Modulo 3 - Dominando a linguagem de programação JavaScript/Sintaxe Básica em Javascript/aula2 - variaveis/script.js
@@ -0,0 +1,69 @@
+// tipos primitivos
+
+// boolean
+var vOuF = false;
+console.log(typeof(vOuF))
+
+ // number
+var numQualquer = 2;
+console.log(typeof(numQualquer))
+
+// string
+var nome = 'pedro';
+// console.log(typeof(nome))
+
+// como declarar varivaeis
+// tres pissibilidades: const - constante, let - escopo local, var - escopo global
+
+// comparação : ==
+var comparação = 0 == 0;
+console.log(comparação)
+
+// comparação identica: compara os valores e os tipos
+var comparacaoIdentica = 0 === "000"
+console.log(comparacaoIdentica)
+
+
+// Operadores aritiméticos:  + adição, - subtração, * multiplicação, / divisão, % divisão inteira, ** potenciação
+
+// adição
+var adicao = 1+1;
+console.log(adicao)
+
+// subtração
+var subtracao = 1-1
+console.log(subtracao)
+
+// multiplicação 
+var multiplicacao = 2 * 3
+console.log(multiplicacao)
+
+// divisao real
+var divisaoReal  = 6 / 2
+console.log(divisaoReal)
+
+// divisão inteira
+var divisaoInteira = 5 % 2
+console.log(divisaoInteira)
+
+// pontenciação
+var pontenciacao = 2 ** 10
+console.log(pontenciacao)
+
+// Operadores relacionais: > maior que, < menor que, >= maior ou igual a, <= menor ou igual a
+
+// maior que
+var maiorQue = 5 > 2
+console.log(maiorQue)
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue)
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2
+console.log(maiorOuIgual)
+
+// menor ou igual a
+var menorOuIgual = 5 <= 2
+console.log(menorOuIgual)
